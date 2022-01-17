279f8a1c2b56d9e0f6f45e4fbd6819f35ea54947
Clay Stewart <c.stewart89@gmail.com>
2022-01-17 18:08:08 -0500

    commit tree 2e1159e117233c1a3b98833a47043cd100cc4d31

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+Rails
+  .application
+  .routes
+  .draw do
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :authors, only: %i[new create show edit update]
+    resources :posts, only: %i[new create show edit update]
+  end
